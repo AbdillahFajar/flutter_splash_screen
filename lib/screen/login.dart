af1349cfddabe210018596c3064e43143c1f4740
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-10-31 13:46:10 +0700

    halaman login, selesai!

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -10,7 +10,7 @@ class Login extends StatelessWidget {
         child: SingleChildScrollView(
           child: Container(
             padding: const EdgeInsets.all(20.0),
-            child:  Column(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(height: 10),
@@ -24,7 +24,7 @@ class Login extends StatelessWidget {
                     fontSize: 25,
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
-                  )
+                  ),
                 ),
 
                 SizedBox(height: 10),
@@ -34,7 +34,7 @@ class Login extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 12.5,
                     fontWeight: FontWeight.bold,
-                    color: Colors.black
+                    color: Colors.black,
                   ),
                 ),
 
@@ -45,27 +45,115 @@ class Login extends StatelessWidget {
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: 'Email',
-                    labelStyle: TextStyle(color: Colors.black),
-                    hintText: 'Masukan alamat email Anda',
-                    prefixIcon: Icon(Icons.email, color: Colors.black),
+                    hintText: 'Masukkan alamat email Anda',
+                    prefixIcon: const Icon(Icons.email),
                     //tambah border ketika field gak diklik user
                     enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: BorderSide(
-                        color: Colors.black,
-                        width: 1
-                      )
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.black),
                     ),
                     //tambah border ketika field diklik user
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide(
-                        color: Colors.amber,
-                        width: 2
-                      )
-                    )
+                      borderSide: BorderSide(color: Colors.amber, width: 2),
+                    ),
                   ),
-                )
+                ),
+
+                SizedBox(height: 20),
+
+                //TextField Password
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukkan password Anda',
+                    prefixIcon: const Icon(Icons.lock_outlined),
+                    suffixIcon: const Icon(Icons.visibility_outlined),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.amber, width: 2),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Lupa Password?',
+                      style: TextStyle(color: Colors.blue[700], fontSize: 14),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        foregroundColor: Colors.black,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(12),
+                        ),
+                        elevation: 2,
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+
+                    //bikin garis yang ada tulisan 'atau' di tengahnya
+                    Row(
+                      children: [
+                        Expanded(child: Divider(color: Colors.grey[300])),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            'atau',
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ),
+                        Expanded(child: Divider(color: Colors.grey[300])),
+                      ],
+                    ),
+
+                    SizedBox(height: 20),
+                    //Bikin tombol untuk sosial media
+                    OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.g_mobiledata, color: Colors.grey[800]),
+                      label: Text(
+                        'Login dengan Google',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          side: BorderSide(color: Colors.grey[300]!),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
